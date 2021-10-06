228ab51a4d9a4f2edc3bc39aee452552b2f62072
tamalkrbose <bosetk1997@gmail.com>
2021-10-07 01:28:06 +0530

    l

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,32 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: 'dashboard',
     pathMatch: 'full'
   },
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'folder',
+    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'records',
+    loadChildren: () => import('./pages/records/records.module').then( m => m.RecordsPageModule)
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./pages/dashboard/dashboard.module').then( m => m.DashboardPageModule)
+  },
+  {
+    path: 'print',
+    loadChildren: () => import('./pages/print/print.module').then( m => m.PrintPageModule)
   }
 ];
 
---
+++ src/app/pages/print/print.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-print',
+  templateUrl: './print.page.html',
+  styleUrls: ['./print.page.scss'],
+})
+export class PrintPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
