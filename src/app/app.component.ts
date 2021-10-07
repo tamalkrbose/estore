94c3bb3a1d95e55418225d8851fb46f4abfe54e4
tamalkrbose <bosetk1997@gmail.com>
2021-10-07 10:14:06 +0530

    k

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,22 @@
 import { Component } from '@angular/core';
+import firebase from 'firebase/app';
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBeRZX8ixxLK7IM-VYokTZ-ZW_AthSYMDE",
+  authDomain: "erp-document.firebaseapp.com",
+  projectId: "erp-document",
+  storageBucket: "erp-document.appspot.com",
+  messagingSenderId: "289446363125",
+  appId: "1:289446363125:web:72234e71f47d72870b2abc",
+  measurementId: "G-E1HSL6N6B8"
+};
+ //var app = firebase.initializeApp(firebaseConfig);
+ if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}else {
+  firebase.app(); // if already initialized, use that one
+}
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
